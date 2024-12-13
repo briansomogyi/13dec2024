3d803e909ee9094a041c3fdd400bbea3b54a2622
briansomogyi <brian.somogyi@emanuel.ro>
2024-12-13 12:11:15 +0200

    Added "ButtonClick.vue" and "HelloWorld.vue" components

---
+++ src/components/ButtonClick.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps(["startCount"])
+const clicks = ref(props.startCount);
+function countClicks() {
+  clicks.value++;
+}
+</script>
+
+<template>
+  <button type="button" @click="countClicks">Count {{ clicks }}</button>
+</template>
