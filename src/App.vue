394cb4d2262bd4856b9d8dabf17d18ade3f29336
briansomogyi <brian.somogyi@emanuel.ro>
2024-12-13 13:48:20 +0200

    Added the "InputConversie.vue" component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { ref } from "vue";
 import ButtonClick from "./components/ButtonClick.vue"
 import HelloWorld from "./components/HelloWorld.vue"
+import InputConversie from "./components/InputConversie.vue";
+
+const total = ref(0);
+function conversion(value) {
+    total.value = value * 4.9750;
+}
 </script>
 
 <template>
-    <HelloWorld greeting="Hello World!" />
-    <HelloWorld greeting="Do you like Vue ?" />
-    <ButtonClick v-for="i in Array(10)" />
+    <!-- <ButtonClick v-for="buttons in Array(5)" /> -->
+    <!-- <ButtonClick :startCount="7" /> -->
+    <!-- <ButtonClick :startCount="5" /> -->
+    <!-- <ButtonClick :startCount="3" /> -->
+    <br>
+    <InputConversie :labelInput="'Suma pe care dorești sa o schimbi'" :currency="'EUR'" :disabled="false"
+        @inputValue="conversion" />
+    <br />
+    <InputConversie :labelInput="'Valoare'" :currency="'RON'" :disabled="true" :total="total" />
 </template>
---
+++ src/components/InputConversie.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['labelInput', 'currency', 'disabled', 'total'])
+const emit = defineEmits(['inputValue'])
+
+function eventHandler(event) {
+    emit('inputValue', event.target.value)
+}
+
+</script>
+
+<template>
+    <label for="valoare">{{ labelInput }}</label><br />
+    <input type="text" id="valoare" :disabled="disabled" :value="total" @input="eventHandler"><span>{{ currency
+        }}</span>
+</template>
